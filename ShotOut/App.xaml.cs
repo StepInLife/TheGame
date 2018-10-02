b31758f41eb0f40ce20ad50535dd01e7d93c9578
LeonGann <ganzenko@itstep.org>
2018-10-02 20:10:44 +0300

    add sources

---
+++ ShotOut/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ShotOut.ViewModels;
+using ShotOut.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ShotOut
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var v = new MainView();
+            var vm = new MainViewModel();
+            v.DataContext = vm;
+            MainWindow = v;
+            v.Show();
+        }
+    }
+}
